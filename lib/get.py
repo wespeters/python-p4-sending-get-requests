e011e5d1d2b6f3d4fc2af85bd645832248294324
Wesley Peters <wesleypeters@gmail.com>
2023-07-25 15:56:42 -0600

    add dummyjson url

--- lib/get.py
+++ lib/get.py
@@ -3,6 +3,8 @@ import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
+# url = "https://dummyjson.com/products"
+
 response = requests.get(url)
 
 json_content = json.loads(response.text)
